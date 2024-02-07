d7173291f6ebe4bc256e94025c45d25a75351664
krlkonstantine <krlkonstantine@gmail.com>
2024-02-07 08:08:18 +0300

    small changes

--- src/components/2024/02_Feb/WhatCenturyIsIt.js
+++ src/components/2024/02_Feb/WhatCenturyIsIt.js
@@ -24,22 +24,22 @@ console.log(whatCentury("2154"))
 console.log(whatCentury("2259"))
 console.log(whatCentury("2000"))
 
-function whatCentury(year) {
-    var century = Math.ceil(year / 100);
-    return century + (century < 20 ? 'th' : ['th', 'st', 'nd', 'rd'][century % 10] || 'th');
-}
-
-function whatCentury(year) {
-    var century = Math.ceil(year / 100);
-    if (century > 10 && century < 20) return century + "th";
-    switch (century % 10) {
-        case 1:
-            return century + "st";
-        case 2:
-            return century + "nd";
-        case 3:
-            return century + "rd";
-        default:
-            return century + "th";
-    }
-}
+// function whatCentury(year) {
+//     var century = Math.ceil(year / 100);
+//     return century + (century < 20 ? 'th' : ['th', 'st', 'nd', 'rd'][century % 10] || 'th');
+// }
+//
+// function whatCentury(year) {
+//     var century = Math.ceil(year / 100);
+//     if (century > 10 && century < 20) return century + "th";
+//     switch (century % 10) {
+//         case 1:
+//             return century + "st";
+//         case 2:
+//             return century + "nd";
+//         case 3:
+//             return century + "rd";
+//         default:
+//             return century + "th";
+//     }
+// }
